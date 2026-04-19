04c89cf7e1323c1e02eaba2af398bdded579efd1
Alireza Sheykholeslami <sheykholeslami99@gmail.com>
2026-04-19 14:56:05 +0800

    feat: enable screen freezing and screens in index.js

--- index.js
+++ index.js
@@ -5,5 +5,9 @@
 import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
+import { freezeEnabled, screensEnabled } from 'react-native-screens';
+
+screensEnabled(true);
+freezeEnabled(true);
 
 AppRegistry.registerComponent(appName, () => App);
