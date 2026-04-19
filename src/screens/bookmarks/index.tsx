f2df1dd74853d7b58f68dbe6e7192692ca41373e
Alireza Sheykholeslami <sheykholeslami99@gmail.com>
2026-04-19 14:53:39 +0800

    refactor: simplify top stories retrieval by using custom hook

--- src/screens/bookmarks/index.tsx
+++ src/screens/bookmarks/index.tsx
@@ -1,22 +1,19 @@
 import { StoriesList } from '@/components/storiesList';
-import { QUERY_KEYS } from '@/hooks/queries/keys';
-import { StoryDto } from '@/hooks/queries/types/story';
+import { useTopStories } from '@/hooks/queries/useTopStories';
 import useBookmarksStore from '@/store/bookmarksStore';
-import { useQueryClient } from '@tanstack/react-query';
 import React, { useMemo } from 'react';
 import { View, StyleSheet } from 'react-native';
 
 const BookmarksScreen = () => {
-  const queryClient = useQueryClient();
-  const topStories: StoryDto[] | undefined = queryClient.getQueryData([
-    QUERY_KEYS.topStories,
-  ]);
+  const { topStories } = useTopStories();
   const bookmarkedStories = useBookmarksStore(
     state => state.bookmarkedStoryIds,
   );
 
   const bookmarkedTopStories = useMemo(() => {
+    console.log({ topStories });
     if (!topStories) return [];
+
     return topStories.filter(story => bookmarkedStories.includes(story.id));
   }, [topStories, bookmarkedStories]);
 
